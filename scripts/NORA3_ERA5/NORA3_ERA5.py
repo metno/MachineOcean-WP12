93343edc42b597c545c2f9833a3381bed9c8dbf7
Martin Lilleeng Sætra <martinls@met.no>
2020-11-26 17:39:58 +0100

    Logic for choosing between ERA5 and NORA3

--- scripts/NORA3_ERA5/NORA3_ERA5.py
+++ scripts/NORA3_ERA5/NORA3_ERA5.py
@@ -6,6 +6,7 @@ Install requirements with pip3 install -r requirements.txt
 #%%
 import netCDF4 as nc4
 import numpy as np
+import os
 import xarray as xr
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
@@ -16,40 +17,56 @@ from dateutil.relativedelta import relativedelta
 def get_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
     """Time series extraction from NORA3 and ERA5.
 
-    WARNING: Only ERA5 implemented.
+    NOTE: Use ERA5 parameter names. See atm_params_nora3 for NORA3 equivalents
 
     Returns tuple with string "NORA3" or "ERA5" depending on which data set was
     used, and time series xarray for parameter param at location (lat, lon) in 
     interval [start_time, end_time] with a temporal resolution of one hour. If 
-    NORA3 data is not available for the given location, ERA5 data will be used. 
-    The nearest grid point will be used. Returns an empty xarray if time 
-    interval is outside the intervals listed under "Data directories".
+    NORA3 data is not available for the given location and the full time 
+    interval, ERA5 data will be used. The nearest grid point will be used.
 
     Data directories: 
     /lustre/storeB/project/fou/om/ERA/ERA5 [1979-1, 2019-12]
     /lustre/storeB/project/fou/om/WINDSURFER/HM40h12/netcdf [1997-08, 2019-12]
     """
-    nora3_da = get_nora3_timeseries(param, lat, lon, start_time, end_time, use_atm)
-    if nora3_da.time.size == 0:
-        return ("ERA5", get_era5_timeseries(param, lat, lon, start_time, end_time, use_atm))
+    available_atm_params = ["msl", "u10", "v10"]
+    available_wave_params = ["msl", "mwd", "swh"]
+
+    atm_params_nora3 = {
+        "msl": "air_pressure_at_sea_level",
+        "u10": "x_wind_10m",
+        "v10": "y_wind_10m"
+    }
+
+    # sanity check arguments
+    if param not in available_atm_params and param not in available_wave_params:
+        raise RuntimeError("Undefined parameter: " + param)
+    if datetime(2019, 12, 31) < start_time or start_time < datetime(1979, 1, 1):
+        raise RuntimeError("Start time outside data set time interval")
+    if datetime(2019, 12, 31) < end_time or end_time < datetime(1979, 1, 1):
+        raise RuntimeError("End time outside data set time interval")
+
+    # inside time interval and domain for NORA3?
+    if start_time >= datetime(1997, 8, 1, 4, 0, 0) and (44.0 <= lat <= 83.0) and (-30.0 <= lon <= 85.0):
+        return ("NORA3", get_nora3_timeseries(atm_params_nora3[param], lat, lon, start_time, end_time))
     else:
-        return ("NORA3", nora3_da)
+        return ("ERA5", get_era5_timeseries(param, lat, lon, start_time, end_time, use_atm))
 
 def get_era5_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
     """Time series extraction from ERA5.
 
     Returns time series xarray for parameter param at location (lat, lon) in interval
     [start_time, end_time] with a temporal resolution of one hour. The nearest 
-    grid point will be used. Returns an empty xarray if time interval is outside
-    the intervals listed under "Data directories".
+    grid point will be used.
 
     Data directories: 
     /lustre/storeB/project/fou/om/ERA/ERA5 [1979-1, 2019-12]
     """
-
-    # sanity check arguments
+    data_dir = "/lustre/storeB/project/fou/om/ERA/ERA5"
     available_atm_params = ["msl", "u10", "v10"]
     available_wave_params = ["msl", "mwd", "swh"]
+    
+    # sanity check arguments
     if param not in available_atm_params and param not in available_wave_params:
         raise RuntimeError("Undefined parameter: " + param)
     if -90.0 > lat > 90.0:
@@ -68,10 +85,10 @@ def get_era5_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
 
     if param in available_atm_params and use_atm:
         for month in (start_time + relativedelta(months=+n) for n in range(month_count)):
-            filenames.append("/lustre/storeB/project/fou/om/ERA/ERA5/atm/era5_atm_CDS_{}.nc".format(month.strftime("%Y%m")))
+            filenames.append(os.path.join(data_dir, "atm/era5_atm_CDS_{}.nc".format(month.strftime("%Y%m"))))
     elif param in available_wave_params:
         for month in (start_time + relativedelta(months=+n) for n in range(month_count)):
-            filenames.append("/lustre/storeB/project/fou/om/ERA/ERA5/wave/era5_wave_CDS_{}.nc".format(month.strftime("%Y%m")))
+            filenames.append(os.path.join(data_dir, "wave/era5_wave_CDS_{}.nc".format(month.strftime("%Y%m"))))
     else:
         raise RuntimeError(param + " is not found in ERA5 wave data set (try use_atm=True)")
     
@@ -84,20 +101,20 @@ def get_era5_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
     # return time series as xarray
     return era5_da
 
-def get_nora3_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
+def get_nora3_timeseries(param, lat, lon, start_time, end_time):
     """Time series extraction from NORA3.
 
     Returns time series xarray for parameter param at location (lat, lon) in interval
     [start_time, end_time] with a temporal resolution of one hour. The nearest 
-    grid point will be used. Returns an empty xarray if time interval is outside
-    the intervals listed under "Data directories".
+    grid point will be used.
 
     Data directories: 
     /lustre/storeB/project/fou/om/WINDSURFER/HM40h12/netcdf [1997-08, 2019-12]
     """
+    data_dir = "/lustre/storeB/project/fou/om/WINDSURFER/HM40h12/netcdf"
+    available_atm_params = ["air_pressure_at_sea_level", "x_wind_10m", "y_wind_10m"]
 
     # sanity check arguments
-    available_atm_params = ["air_pressure_at_sea_level", "x_wind_10m", "y_wind_10m"]
     if param not in available_atm_params:
         raise RuntimeError("Undefined parameter: " + param)
     if 44.0 > lat > 83.0:
@@ -105,8 +122,8 @@ def get_nora3_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
     if -30.0 > lon > 85.0:
         raise RuntimeError("Longitude (lon) must be in the interval [-30.0, 85.0]")
     
-    print("From " + start_time.strftime("%Y%m%d-%H"))
-    print("To " + end_time.strftime("%Y%m%d-%H"))
+    #print("From " + start_time.strftime("%Y%m%d-%H"))
+    #print("To " + end_time.strftime("%Y%m%d-%H"))
 
     # find and open correct netCDF file(s)
     filenames = []
@@ -133,21 +150,27 @@ def get_nora3_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
                 index_file += 24
 
             filenames.append(
-                "/lustre/storeB/project/fou/om/WINDSURFER/HM40h12/netcdf/{year}/{month}/{day}/{period:02d}/fc{year}{month}{day}{period:02d}_00{index_file}_fp.nc" \
+                os.path.join(data_dir, "{year}/{month}/{day}/{period:02d}/fc{year}{month}{day}{period:02d}_00{index_file}_fp.nc" \
                 .format(year=current_time_with_offset.strftime("%Y"), 
                         month=current_time_with_offset.strftime("%m"), 
                         day=current_time_with_offset.strftime("%d"), 
-                        period=period, index_file=index_file))
+                        period=period, index_file=index_file)))
             current_time += hour
     else:
         raise RuntimeError(param + " is not found in NORA3 data set")
     
-    print(filenames)
     nora3 = xr.open_mfdataset(filenames)
 
     # find coordinates in data set projection
-    data_crs = ccrs.LambertConformal(central_longitude=-42.0) #, standard_parallels=[66.3])
+    data_crs = ccrs.LambertConformal(central_longitude=-42.0, central_latitude=66.3,
+                standard_parallels=[66.3, 66.3], 
+                globe=ccrs.Globe(datum="WGS84", ellipse="WGS84",
+                semimajor_axis=6371000.0, semiminor_axis=6371000.0))
     x, y = data_crs.transform_point(lon, lat, src_crs=ccrs.PlateCarree())
+
+    #nora3_da_lon = nora3["longitude"].sel(x=x, y=y, method="nearest")
+    #nora3_da_lat = nora3["latitude"].sel(x=x, y=y, method="nearest")
+    #print("Projected lon, lat: " + str(nora3_da_lon.values) + ", " + str(nora3_da_lat.values))
 
     # extract data set
     nora3_da = nora3[param].sel(x=x, y=y, method="nearest")
@@ -159,9 +182,9 @@ def get_nora3_timeseries(param, lat, lon, start_time, end_time, use_atm=True):
     # return time series as xarray
     return nora3_da
 
-# test time series extraction and plotting from ERA5 data set
-ds, da = get_timeseries("x_wind_10m", 58.0, 10.0, 
-        datetime(2019, 2, 1, 0, 0, 0), datetime(2019, 2, 1, 12, 0, 0))
+# test time series extraction and plotting from NORA3/ERA5 data set
+ds, da = get_timeseries("u10", 58.0, 20.0, 
+        datetime(1997, 8, 1, 0, 0, 0), datetime(1997, 8, 1, 2, 0, 0))
 print(ds)
 print(da)
 print(da.values[0]) # .values is a numpy.ndarray
